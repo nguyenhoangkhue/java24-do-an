84c4055d758baf9a600c05eb3814475bea7c8946
nguyenhoangkhue <snh110685@gmail.com>
2023-11-15 22:20:06 +0700

    upd

---
+++ Do-An-java24/assets/js/custom-owl.js
@@ -0,0 +1,22 @@
+$(document).ready(function(){
+    $('.owl-carousel').owlCarousel({
+      loop:true,
+      margin:20,
+      nav: true,
+      dots: true,
+      autoplay: true,
+      autoplayTimeout: 2000,
+      responsive:{
+          0:{
+              items:1
+          },
+          600:{
+              items:2
+          },
+          1000:{
+              items:4
+          }
+      }
+  })
+    });
+  
